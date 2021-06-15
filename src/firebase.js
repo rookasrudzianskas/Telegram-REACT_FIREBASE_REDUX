e6849c3e098a7ab1005f9b3a9d40a6e6dec589e1
Rokas <rookas.rudzenskas@gmail.com>
2021-06-15 15:29:15 +0300

    Roaks

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBik6JvfY31VapKrHV9n0Qd-HZPdhbShe8",
+    authDomain: "rookas-telegram-app.firebaseapp.com",
+    projectId: "rookas-telegram-app",
+    storageBucket: "rookas-telegram-app.appspot.com",
+    messagingSenderId: "510982523109",
+    appId: "1:510982523109:web:ce261e8d1d58bcb0e52c0c",
+    measurementId: "G-3TE3CR5PS4"
+};
