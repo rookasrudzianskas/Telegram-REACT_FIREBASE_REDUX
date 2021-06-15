45b2bce78315a1db25adaaa32b91b47b7ad6caf0
Rokas <rookas.rudzenskas@gmail.com>
2021-06-15 15:38:03 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Telegram from "./components/Telegram";
 
 function App() {
   return (
     <div className="app">
-
+      <Telegram />
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Sidebar = () => {
     return (
         <div className="sidebar">
-
+            Sidebar
         </div>
     );
 };
