866118b6d48ca5fb0c9ce0a85e8d9a9c862ed999
Rokas <rookas.rudzenskas@gmail.com>
2021-06-17 09:45:30 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Telegram from "./components/Telegram";
+import Login from "./components/Login";
+import {useSelector} from "react-redux";
+import {selectUser} from "./features/userSlice";
 
 function App() {
+
+  const user = useSelector(selectUser);
+
+  useEffect(() => {
+
+  }, [dispatch]);
+
   return (
     <div className="app">
-      <Telegram />
+        {!user ? (
+            <Login />
+        ) : (
+            <Telegram />
+        )}
+
     {/*    does not work*/}
     </div>
   );
