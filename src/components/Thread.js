e0735f0a7a08973a5e8c872dc1f10c3ff8ce96c9
Rokas <rookas.rudzenskas@gmail.com>
2021-06-17 12:10:24 +0300

    Rokas

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -59,7 +59,7 @@ const Thread = () => {
                 <div className="thread__header__contents">
                     <Avatar />
                     <div className="thread__header__contents__info">
-                        <h4>ThreadName</h4>
+                        <h4>{threadName}</h4>
                         <h5>Last seen</h5>
                     </div>
                 </div>
