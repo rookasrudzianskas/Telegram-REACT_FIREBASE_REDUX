dfaa1299a0fa8c402b8ecc4ef3bd3b36d4f58acc
Rokas <rookas.rudzenskas@gmail.com>
2021-06-17 10:02:48 +0300

    Rokas

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import "./styles/Login.css";
+import {Button} from "@material-ui/core";
+import {auth, provider} from "../firebase";
 
 const Login = () => {
+
+    const signIn = () => {
+        auth.signInWithPopup(provider).catch((error) => alert(error.message));
+    }
+
     return (
         <div className="login">
+            <div className="login__telegram">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Telegram_Messenger.png" alt=""/>
 
+                <h1>Telegram</h1>
+            </div>
+
+            <Button onClick={signIn}>Sign In To Telegram</Button>
         </div>
     );
 };
