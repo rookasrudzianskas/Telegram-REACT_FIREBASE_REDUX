d8120558254eec372bd38c16f15e81ee05b60bd9
Rokas <rookas.rudzenskas@gmail.com>
2021-06-17 09:52:23 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React, {useEffect} from 'react';
 import './App.css';
 import Telegram from "./components/Telegram";
 import Login from "./components/Login";
-import {useSelector} from "react-redux";
-import {selectUser} from "./features/userSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {login, logout, selectUser} from "./features/userSlice";
+import {auth} from "./firebase";
 
 function App() {
 
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        // login here
+        dispatch(login({
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          displayName: authUser.displayName,
+        }))
 
+      } else {
+        // logout here
+
+        dispatch(logout());
+      }
+    })
   }, [dispatch]);
 
   return (
